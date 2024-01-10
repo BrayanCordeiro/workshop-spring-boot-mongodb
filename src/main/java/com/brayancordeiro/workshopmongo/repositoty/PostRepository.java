2d73f8e36973cc480c597c7930cf50fd7e6675be
BrayanCordeiro <linsbvl73@gmail.com>
2024-01-10 03:09:22 -0300

    Consulta simples com @Query

--- src/main/java/com/brayancordeiro/workshopmongo/repositoty/PostRepository.java
+++ src/main/java/com/brayancordeiro/workshopmongo/repositoty/PostRepository.java
@@ -3,14 +3,18 @@ package com.brayancordeiro.workshopmongo.repositoty;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.brayancordeiro.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-
-	//Nesse ponto o springdata monta a consulta
+	
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
